5679570340e2f8c8ca06cff0bee077d3262fda4a
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2024-12-28 18:11:11 +0700

    feat: add PrimaryButton widget

---
+++ lib/widgets/primary_button.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class PrimaryButton extends StatelessWidget {
+  const PrimaryButton({
+    super.key,
+    required this.text,
+    this.onPressed,
+  });
+
+  final String text;
+  final void Function()? onPressed;
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    final textTheme = theme.textTheme;
+    final colorScheme = theme.colorScheme;
+
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        backgroundColor: colorScheme.primary,
+        foregroundColor: colorScheme.onPrimary,
+        textStyle: textTheme.labelMedium,
+      ),
+      child: Text(text),
+    );
+  }
+}
