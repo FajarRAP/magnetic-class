3f43789bc743582d81d3232a4b7ac2261a822a5c
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2025-01-04 19:16:17 +0700

    feat: update routing to use items page and handle dynamic arguments

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'constants.dart';
 import 'pages/home_page.dart';
-import 'pages/tools_page.dart';
+import 'pages/items_page.dart';
 import 'themes/theme.dart';
 
 void main() => runApp(const MyApp());
@@ -15,10 +16,22 @@ class MyApp extends StatelessWidget {
       initialRoute: '/',
       routes: {
         '/': (context) => const HomePage(),
-        '/tools': (context) => const ToolsPage(),
+      },
+      onGenerateRoute: (settings) {
+        if (settings.name == '/tools') {
+          final args = settings.arguments as Map<String, dynamic>;
+          final title = args['title'] as String;
+          final items = args['items'] as List<Item>;
+
+          return MaterialPageRoute(
+            builder: (context) => ItemsPage(items: items, title: title),
+          );
+        }
+
+        return null;
       },
       theme: theme,
       title: 'Magnetic Class',
     );
   }
-}
+} 
