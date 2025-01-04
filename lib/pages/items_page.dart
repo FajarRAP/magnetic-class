254b13ffb53b091a1e7b229496106e19fed95df8
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2025-01-04 19:15:33 +0700

    refactor (tools page): change tools page to items page

--- lib/pages/items_page.dart
+++ lib/pages/items_page.dart
@@ -2,29 +2,34 @@ import 'package:flutter/material.dart';
 import 'package:magnetic_class/constants.dart';
 import 'package:magnetic_class/widgets/horizontal_image_and_title.dart';
 
-class ToolsPage extends StatelessWidget {
-  const ToolsPage({super.key});
+class ItemsPage extends StatelessWidget {
+  const ItemsPage({
+    super.key,
+    required this.title,
+    required this.items,
+  });
+
+  final String title;
+  final List<Item> items;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Alat Eksperimen'),
-      ),
+      appBar: AppBar(title: Text(title)),
       body: ListView.separated(
         itemBuilder: (context, index) {
           String match(Match? match) => '-';
-          final url = tools[index].imageUrl;
-          final loweredTitle = tools[index].title.toLowerCase();
+          final url = items[index].imageUrl;
+          final loweredTitle = items[index].title.toLowerCase();
           final slug = loweredTitle.splitMapJoin(' ', onMatch: match);
           return HorizontalImageAndTitle(
-            description: tools[index].description,
-            title: tools[index].title,
+            description: items[index].description,
+            title: items[index].title,
             imageUrl: '$url$slug.png',
           );
         },
         separatorBuilder: (context, index) => const SizedBox(height: 12),
-        itemCount: tools.length,
+        itemCount: items.length,
         padding: const EdgeInsets.all(16),
       ),
     );
