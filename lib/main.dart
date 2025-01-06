2270531ad04c14c6e3144a22be00c10a8e61a661
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2025-01-06 21:20:30 +0700

    refactor(main.dart): streamline routing by removing static routes and using onGenerateRoute

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 
-import 'constants.dart';
-import 'pages/home_page.dart';
-import 'pages/items_page.dart';
+import 'routes.dart';
 import 'themes/theme.dart';
 
 void main() => runApp(const MyApp());
@@ -13,25 +11,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      onGenerateRoute: onGenerateRoute,
       initialRoute: '/',
-      routes: {
-        '/': (context) => const HomePage(),
-      },
-      onGenerateRoute: (settings) {
-        if (settings.name == '/tools') {
-          final args = settings.arguments as Map<String, dynamic>;
-          final title = args['title'] as String;
-          final items = args['items'] as List<Item>;
-
-          return MaterialPageRoute(
-            builder: (context) => ItemsPage(items: items, title: title),
-          );
-        }
-
-        return null;
-      },
       theme: theme,
       title: 'Magnetic Class',
     );
   }
-} 
+}
