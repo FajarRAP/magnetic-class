0813ebf8ec776759f2b7c9f19b90d965ced24389
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2025-01-06 22:22:48 +0700

    feat: add routing for experiment steps page

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,22 +1,24 @@
 import 'package:flutter/material.dart';
 
 import 'constants.dart';
+import 'pages/experiment_steps_page.dart';
 import 'pages/home_page.dart';
 import 'pages/items_page.dart';
 
+MaterialPageRoute toRoute(Widget page) =>
+    MaterialPageRoute(builder: (context) => page);
 Route<dynamic>? onGenerateRoute(RouteSettings settings) {
-  MaterialPageRoute toRoute(Widget page) =>
-      MaterialPageRoute(builder: (context) => page);
-
   switch (settings.name) {
-    case '/':
+    case homeRoute:
       return toRoute(const HomePage());
-    case '/tools':
+    case experimentItemsRoute:
       final args = settings.arguments as Map<String, dynamic>;
       final title = args['title'] as String;
       final items = args['items'] as List<Item>;
 
       return toRoute(ItemsPage(title: title, items: items));
+    case experimentStepsRoute:
+      return toRoute(const ExperimentStepsPage());
     default:
       return null;
   }
