f0eb34c78e24a41bf189eee2e868a2d8dd2f7f35
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2025-01-20 17:08:19 +0700

    feat (experiment steps page): add video step

--- lib/pages/experiment_steps_page.dart
+++ lib/pages/experiment_steps_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import '../constants.dart';
 import '../widgets/experiment_steps_item.dart';
+import '../widgets/video_steps.dart';
 
 class ExperimentStepsPage extends StatelessWidget {
   const ExperimentStepsPage({super.key});
@@ -10,6 +11,7 @@ class ExperimentStepsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
     final textTheme = theme.textTheme;
+    final colorScheme = theme.colorScheme;
 
     return Scaffold(
       appBar: AppBar(title: const Text('Langkah Eksperimen')),
@@ -61,7 +63,52 @@ class ExperimentStepsPage extends StatelessWidget {
             imageUrl: '${characterizationUrl}1.jpg',
             description:
                 'mencelupkan sampel ke dalam cairan nitrogen dan diukur perubahan suhu terhadap potensial dengan LoggerPro',
-          )
+          ),
+          const SizedBox(height: 50),
+          Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(12),
+              boxShadow: const [
+                BoxShadow(
+                  blurRadius: 4,
+                  color: Color(0xFFBDBDBD),
+                  offset: Offset(0, 2),
+                ),
+              ],
+              color: Colors.grey.shade300,
+            ),
+            child: Column(
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: const BorderRadius.vertical(
+                      top: Radius.circular(12),
+                    ),
+                    color: colorScheme.primaryContainer,
+                  ),
+                  padding: const EdgeInsets.all(8),
+                  width: double.infinity,
+                  child: Text(
+                    'Video Langkah Eksperimen',
+                    style: textTheme.labelLarge
+                        ?.copyWith(fontWeight: FontWeight.w500),
+                  ),
+                ),
+                const Padding(
+                  padding: EdgeInsets.all(12),
+                  child: VideoSteps(),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 12),
+                  child: Text(
+                    'Eksperimen pembuatan sensor suhu pada lapisan magnetik Cu/NiFe berbantuan medan magnet sejajar',
+                    style: textTheme.labelMedium,
+                  ),
+                ),
+                const SizedBox(height: 12),
+              ],
+            ),
+          ),
         ],
       ),
     );
