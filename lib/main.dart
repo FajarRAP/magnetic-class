d9a5f1d45d927386669320b389784c62ad3a1f56
Fajar Riansyah Aryda Putra <fajary781@gmail.com>
2025-02-12 09:35:03 +0700

    chore (main): change initial route to splash route

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       onGenerateRoute: onGenerateRoute,
-      initialRoute: homeRoute,
+      initialRoute: splashRoute,
       theme: theme,
       title: 'Magnetic Class',
     );
